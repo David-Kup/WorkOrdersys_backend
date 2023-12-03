d2ab2a8ce9d50e7b08cdd592b3d3854166f9ffde
Natural <128178042+naturalspirit-dev@users.noreply.github.com>
2023-12-04 07:01:00 +0800

    update: Ip Restrict

--- apps/wo_iprestrict/urls.py
+++ apps/wo_iprestrict/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('/<int:pk>', views.ip_range_detail, name='ip_range_detail'),
     path('/<int:pk>/update', views.ip_range_update, name='ip_range_update'),
     path('/<int:pk>/delete', views.ip_range_delete, name='ip_range_delete'),
+    path('/turn', views.ip_restrict_turn, name='ip_restrict_turn')
 ]
